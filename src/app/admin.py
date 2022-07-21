60acbdb8aebd94b0d7ea1113012c58304bfe1249
Amir Shabani <amir@shabani.dev>
2022-07-21 12:16:49 +0430

    Start an app with a base model

---
+++ src/app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import BaseModel
+
+admin.site.register(BaseModel)
---
+++ src/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from uuid import uuid4
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
