710c75ac3aa28ee74c2baf2d46ef39c6e4fc51c8
Amir Shabani <amir@shabani.dev>
2022-07-23 09:51:03 +0430

    Return proper status codes for POST requests

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import UserSerializer, AddressSerializer, RecentAddressesSerializer
 from app import database
 
@@ -50,6 +51,8 @@ class AddressAPI(APIView):
     def post(self, request):
         serializer = AddressSerializer(data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
